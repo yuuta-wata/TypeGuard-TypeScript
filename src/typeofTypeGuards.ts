02de3b80bcee8c04ebdf088a9437085f90af91a2
yuuta <watanabe@unlog.co.jp>
2021-09-12 18:50:28 +0900

    fix code

--- src/typeofTypeGuards.ts
+++ src/typeofTypeGuards.ts
@@ -1,14 +1,11 @@
 export const TypeofTypeGuard = (value: number | string | boolean) => {
   if (typeof value === 'number') {
     console.log('typeof:', typeof value)
-    return 0
   }
   if (typeof value === 'string') {
     console.log('typeof:', typeof value)
-    return ''
   }
   if (typeof value === 'boolean') {
     console.log('typeof:', typeof value)
-    return false
   }
 }
